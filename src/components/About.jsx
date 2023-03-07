91e1bc03f038e00914c44843c4cc015cced55011
Tamarapreye-M <preyemichael116@gmail.com>
2023-03-07 23:27:45 +0100

    testing

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Footer from "./Home_page/Footer";
+import Navbar from "./Home_page/Navbar";
 
 const About = () => {
-	return <div>About</div>;
+	return (
+		<>
+			<Navbar />
+			<div>About</div>
+			<Footer />
+		</>
+	);
 };
 
 export default About;
