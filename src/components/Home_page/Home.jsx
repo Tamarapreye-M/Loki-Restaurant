194e9ff5eb737f03b58e9563e16ca4519a554ddc
Tamarapreye-M <preyemichael116@gmail.com>
2023-03-07 23:28:46 +0100

    Home page uupdated

---
+++ src/components/Home_page/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Main from "./HomeMain";
+import Navbar from "./Navbar";
+
+const Home = () => {
+	return (
+		<>
+			<Navbar />
+			<Hero />
+			<Main />
+			<Footer />
+		</>
+	);
+};
+
+export default Home;
