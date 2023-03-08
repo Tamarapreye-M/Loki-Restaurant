064784fc3786a0681fa20e374c87867a0eb7ab6b
Tamarapreye-M <preyemichael116@gmail.com>
2023-03-08 20:09:16 +0100

    Implemented react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-import Home from "./components/Home_page/Home";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Pages/Home_page/Home";
+import About from "./components/Pages/About_Page/About";
+import Menu from "./components/Pages/Menu_Page/Menu";
+import Contact from "./components/Pages/Contact_Page/Contact";
 
 const App = () => {
 	return (
 		<div className="">
-			<Home />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/menu" element={<Menu />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/contact" element={<Contact />} />
+			</Routes>
 		</div>
 	);
 };
